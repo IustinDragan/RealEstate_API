75f2d1714891bb4a3f42d0771630b22f5f2f897c
nuyonu <nuyonu@gmail.com>
2024-04-09 16:08:08 +0300

    Add DbuUp

---
+++ RealEstate.DbUp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using DbUp;
+
+// var connectionString = args.FirstOrDefault() ??
+//                        Environment.GetEnvironmentVariable("DBUP_CONNECTION_STRING");
+
+var connectionString = "Server=127.0.0.1,1433;Database=RealEstateDatabase2;User=sa;Password=Test123.!;TrustServerCertificate=true;";
+
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var builder =
+    DeployChanges.To.SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), (scriptName) => !scriptName.Contains("ManualRun"))
+        .LogToConsole();
+
+var result = builder.Build().PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+    #if DEBUG
+    Console.ReadLine();
+    #endif
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
---
+++ RealEstate.DbUp/Scripts/202404091557_Seed_With_Admin_User.sql
@@ -0,0 +1,5 @@
+INSERT INTO Companies(CompanyName, CUI)
+    VALUES('Admin', 'Admin');
+
+INSERT INTO Users(LastName, FirstName, Email, Password, Username, PhoneNumber, Role, CompanyId)
+        VALUES('LastName', 'FirstName', 'admin@admin.com', 'admin', 'admin', '0747123123', 1, 1);
